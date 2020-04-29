0588ed4b3d72debf6361a9518f250ca34d289bcc
Cassie Boyd <cassandra.bacon@gmail.com>
2020-04-29 14:43:40 -0500

    Adding lesson on strings

--- types.js
+++ types.js
@@ -1,1 +1,21 @@
 console.log("It works");
+
+const name = "Cassie";
+const middle = "Andra";
+const last = "Bacon";
+
+const sentence = 'If you don\'t eat your meat you can\'t have any pudding.';
+// When using single or double quotes in variable assignment you have to use \ before any additional single or double quotes contained within your string.
+
+const secondSentence = `We don't need no "education"`;
+// Alternately, using backticks eliminates the need for the escape character altogether!
+
+const song = `Oh
+
+you
+pretty
+
+
+things`
+// Backticks also preserve any line breaks you have in your code.
+
