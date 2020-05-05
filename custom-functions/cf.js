344f61ef8e92cc1c58dac4d5ff50ec35651edc01
Cassie Boyd <cassandra.bacon@gmail.com>
2020-05-05 17:32:49 -0500

    Finished lesson on custom functions

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,1 +1,17 @@
 console.log("It works!")
+
+function calculateBill() {
+    console.log("Running Calculate Bill!")
+    const total = 100 * 1.13;
+    console.log("Console Log", total);
+    return total;
+}
+
+// Capturing the result of the function return in a variable.
+const myTotal = calculateBill();
+console.log(`My Total: $${myTotal}`)
+
+// Does the same as above without the need to capture the return in a variable.
+console.log(`My Total: $${calculateBill()}`)
+
+// console.log(total); // Will cause an error because variable total is only available within the scope (code block) of the calculateBill() function.
