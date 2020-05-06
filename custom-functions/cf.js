e935fbb73c7a1717b172e4f15691ff031a8b09d5
Cassie Boyd <cassandra.bacon@gmail.com>
2020-05-06 17:01:32 -0500

    Starting parameters and arguments lesson

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,17 +1,26 @@
 console.log("It works!")
 
-function calculateBill() {
+
+
+function calculateBill(bill, taxRate) {
     console.log("Running Calculate Bill!")
-    const total = 100 * 1.13;
-    console.log("Console Log", total);
+    const total = bill * (1 + taxRate);
+    // console.log("Console Log", total);
     return total;
 }
 
 // Capturing the result of the function return in a variable.
-const myTotal = calculateBill();
+const myTotal = calculateBill(12.34, .05);
 console.log(`My Total: $${myTotal}`)
 
 // Does the same as above without the need to capture the return in a variable.
-console.log(`My Total: $${calculateBill()}`)
+console.log(`My Total: $${calculateBill(435.54, .10)}`)
 
 // console.log(total); // Will cause an error because variable total is only available within the scope (code block) of the calculateBill() function.
+
+function sayHiTo(firstName) {
+    return `Hello, ${firstName}!`;
+}
+
+const greeting = sayHiTo("Ken");
+console.log(greeting)
