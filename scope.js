5b0e15f3d95520464fd4b09603b4a38f3bb6faa3
Cassie Boyd <cassandra.bacon@gmail.com>
2020-05-14 15:49:14 -0500

    Starting lesson on scope

---
+++ scope.js
@@ -0,0 +1,16 @@
+console.log("It works!");
+const first = "Cassie"; // Global variable
+
+function sayHi() {
+    console.log("hi");
+}
+
+const age = 100;
+
+function go() {
+    const hair = "blonde";
+    console.log(hair);
+}
+
+console.log(age);
+//console.log(hair); // Can't access outside the function it was defined in
