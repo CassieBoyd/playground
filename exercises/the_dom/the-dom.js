387be14e02cc27e6ad08516bd63d7496a6adf331
Cassie Boyd <cassandra.bacon@gmail.com>
2020-05-19 17:10:31 -0500

    Completed lesson on selecting DOM elements

--- exercises/the_dom/the-dom.js
+++ exercises/the_dom/the-dom.js
@@ -1,2 +1,5 @@
-const p = document.querySelector("p");
+const p = document.querySelector("p"); //Selects first matching element
 console.log(p);
+
+const divs = document.querySelectorAll("div"); //Selects all matching elements
+console.log(divs);
