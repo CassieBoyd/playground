5dd5463e9da8decd1fea2740ceb482d13695c293
Cassie Boyd <cassandra.bacon@gmail.com>
2020-05-27 18:17:09 -0500

    Looked a inserting strings of html to the DOM

--- exercises/the_dom/creating_with_strings.js
+++ exercises/the_dom/creating_with_strings.js
@@ -1,10 +1,16 @@
 console.log("It works")
 
 const item = document.querySelector(".item");
-const src = `https://picsum.photos/200`;
-item.innerHTML = `
-    <div>
-        <h1>Hello</h1>
+const width = 500;
+const src = `https://picsum.photos/${width}`;
+const desc =  `Cat`;
+const myHTML = `
+    <div className="wrapper">
+        <h2>Cute ${desc}</h2>
+        <img src="${src}" alt="${desc}"/>
     </div>
-    ` //Backticks allow for multi-line code that maintains the spacing
+`;//Backticks allow for multi-line code that maintains the spacing. 
+item.innerHTML = myHTML;
 console.log(item.innerHTML);
+
+console.log("myHTML is a: ", typeof(myHTML));//myHTML is a string, NOT an element.
