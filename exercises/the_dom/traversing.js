5b0312ae8295d2c4c76ba5191391adb914efed7b
Cassie Boyd <cassandra.bacon@gmail.com>
2020-05-28 11:11:35 -0500

    Completed lesson on traversing and removing nodes

--- exercises/the_dom/traversing.js
+++ exercises/the_dom/traversing.js
@@ -1,11 +1,21 @@
 console.log("It works");
 
 const cassie = document.querySelector(".cassie");
-console.log(cassie.children);
+console.log(cassie.children);//Shows child elements
 
-console.log("child nodes: ", cassie.childNodes);
+console.log("child nodes: ", cassie.childNodes);//Shows text nodes and child elements
+
 console.log("1st element child: ", cassie.firstElementChild);
 console.log("last element child: ", cassie.lastElementChild);
 console.log("previous element sibling: ", cassie.previousElementSibling);
 console.log("next element sibling: ", cassie.nextElementSibling);
 console.log("parent element: ", cassie.parentElement);
+
+// From console, select element then type $0.remove() to remove selected element
+
+const p = document.createElement("p");//Creating p tag
+p.textContent = "I will be removed";//Adding text inside p tag
+document.body.appendChild(p);//Adding p tag to the body
+
+p.remove();
+console.log(p);//Even though it was removed from the DOM we can still access it 
