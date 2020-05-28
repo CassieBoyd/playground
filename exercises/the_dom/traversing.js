a1197cd429d403dc0a65ffaf7dc8825b1394e1f0
Cassie Boyd <cassandra.bacon@gmail.com>
2020-05-28 11:00:44 -0500

    Created traversing.js and started lesson on traversing and removing nodes

---
+++ exercises/the_dom/traversing.js
@@ -0,0 +1,11 @@
+console.log("It works");
+
+const cassie = document.querySelector(".cassie");
+console.log(cassie.children);
+
+console.log("child nodes: ", cassie.childNodes);
+console.log("1st element child: ", cassie.firstElementChild);
+console.log("last element child: ", cassie.lastElementChild);
+console.log("previous element sibling: ", cassie.previousElementSibling);
+console.log("next element sibling: ", cassie.nextElementSibling);
+console.log("parent element: ", cassie.parentElement);
