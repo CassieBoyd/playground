de8e8c9438c8d90845ac68888ecbb4c29bdf8260
Cassie Boyd <cassandra.bacon@gmail.com>
2020-06-04 14:59:46 -0500

    Completed lesson on event listeners

--- exercises/events/events.js
+++ exercises/events/events.js
@@ -8,6 +8,9 @@ function handleClick() {
     console.log("Button was clicked")
 };
 
+// Technically an anonymous function but because it's stored in a variable we can refer to it with methods such as removeEventListener
+const yay = () => console.log("yay");
+
 // Event listener takes two arguments: what event to listen for and what to do once event happens (callback function triggers)
 // Go get something (butts), listen for something(ael), do something (function)
 // Function can be anonymous if defined within the event listener or the event listener can reference a function defined elsewhere
@@ -17,7 +20,37 @@ function handleClick() {
 // });
 
 butts.addEventListener("click", handleClick);
-sibButton.addEventListener("click", handleClick);
+sibButton.addEventListener("click", yay);
 
-// Unbinding handleClick function from butts class elements
+// Unbinding handleClick function from butts class elements. Only works with named functions
 butts.removeEventListener("click", handleClick);
+
+// ****************************************
+// Listen on multiple items
+
+const buyButtons = document.querySelectorAll("button.buy");
+
+console.log(buyButtons);
+
+function buyItem() {
+    console.log("Buying item now");
+}
+
+function loopOverButtons(singleButton) {
+    singleButton.addEventListener("click", buyItem);
+}
+
+buyButtons.forEach(loopOverButtons);
+
+// Function defined inside
+// buyButtons.forEach(function(buyButton) {
+//     console.log("Binding the buy button");
+//     buyButton.addEventListener("click", buyItem);
+// });
+
+// OR
+// buyButtons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//         console.log("You clicked it")
+//     })
+// })
