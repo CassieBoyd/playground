070100dd05db6f9f34b425aa9ecd4c22c6601d57
Cassie Boyd <cassandra.bacon@gmail.com>
2020-06-10 20:01:06 -0500

    Practiced using preventDefault

---
+++ exercises/events/forms.js
@@ -0,0 +1,14 @@
+const wes = document.querySelector(".wes");
+
+wes.addEventListener("click", function(event) {
+    console.log("You clicked it");
+    
+    // confirm() is a method which returns true or false depending on user input
+    const shouldChangePage = confirm("This page may be delicious, do you wish to proceed?");
+    if(!shouldChangePage) {
+        // preventDefault prevents the default action of the event which in this case is to go to the href link
+        event.preventDefault();
+        //window.location = event.currentTarget.href;
+    }
+    console.log(shouldChangePage);
+});
