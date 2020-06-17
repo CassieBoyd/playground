484603b4ee2dddb2640e8da6dc3aa4e5fa8149bb
Cassie Boyd <cassandra.bacon@gmail.com>
2020-06-17 16:29:09 -0500

    Extra info showing in modal

--- exercises/click-outside/click-outside.js
+++ exercises/click-outside/click-outside.js
@@ -1,11 +1,26 @@
 const cardButtons = document.querySelectorAll(".card button");
 
+const modalInner = document.querySelector(".modal-inner");
+const modalOuter = document.querySelector(".modal-outer");
+
+
 function handleCardButtonClick(event) {
     const button = event.currentTarget;
     const card = button.closest(".card");
     // Grab the image src
     const imgSrc = card.querySelector("img").src;
-    console.log(imgSrc);
+    const desc = card.dataset.description;
+    const name = card.querySelector("h2").textContent;
+    // Populate the modal with the new info
+    modalInner.innerHTML = `<img src="${imgSrc.replace("200", "600")}" alt="${name}"/>
+    <p>${desc}</p>
+    `;
+    // Show the modal
+    modalOuter.classList.add("open");
 }
 
 cardButtons.forEach(button => button.addEventListener("click", handleCardButtonClick))
+
+function closeModal() {
+    modalOuter.classList.remove("open");
+}
