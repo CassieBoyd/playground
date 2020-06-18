74f844977da9332616a3ce3290684451d35ca1e8
Cassie Boyd <cassandra.bacon@gmail.com>
2020-06-18 17:04:14 -0500

    Disabled class is removed from button once last element is on page

--- exercises/scroll-to-accept/scroll-to-accept.js
+++ exercises/scroll-to-accept/scroll-to-accept.js
@@ -19,18 +19,30 @@ const terms = document.querySelector(".terms-and-conditions");
 
 const watch = document.querySelector(".watch");
 
+const button = document.querySelector(".accept");
+
+
 // Intersection observer watches if an element is on or off the page
 
-
+// Turns off the disabled class once the last element is on the page
 function obCallback(payload) {
-    console.log(payload);
+    if(payload[0].intersectionRatio === 1) {
+        button.disabled = false;
+    }
+    console.log(payload[0].isIntersecting);
 }
 
-const ob = new IntersectionObserver(obCallback);
+const ob = new IntersectionObserver(obCallback, { 
+    root: terms,
+    threshold: 1,
+});
 
-ob.observe(watch);
+// ob.observe(watch);
 
 terms.addEventListener("scroll", function(event) {
-    console.log(event.currentTarget.scrollTop);
-    console.log(event.currentTarget.scrollHeight);
+    // console.log(event.currentTarget.scrollTop);
+    // console.log(event.currentTarget.scrollHeight);
 });
+
+// Watches for the last element tag to be visible
+ob.observe(terms.lastElementChild);
