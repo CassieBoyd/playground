f39fde22226d828befd0230a69b63a047f7b4c93
Cassie Boyd <cassandra.bacon@gmail.com>
2020-09-04 11:52:28 -0500

    Drawing tiny version of faces

--- exercises/face-detection/dist/face.91655783.js
+++ exercises/face-detection/dist/face.91655783.js
@@ -414,7 +414,9 @@ const canvas = document.querySelector(".video");
 const ctx = canvas.getContext("2d");
 const faceCanvas = document.querySelector(".face");
 const faceCtx = canvas.getContext("2d");
-const faceDetector = new window.FaceDetector(); // Get users' video feed
+const faceDetector = new window.FaceDetector();
+const SIZE = 10; // Any variables that are constant throughout the application are typically named in all caps when working with canvas.
+// Get users' video feed
 
 async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
@@ -437,6 +439,7 @@ async function detect() {
   console.log(faces.length); // Ask browser when next animation frame is and tell it to run detect
 
   faces.forEach(drawFace);
+  faces.forEach(censor);
   requestAnimationFrame();
 }
 
@@ -456,6 +459,24 @@ function drawFace(face) {
   ctx.lineWidth = 2; // Drawing the rectangle with the detected values
 
   ctx.strokeRect(left, top, width, height);
+}
+
+function censor({
+  boundingBox: face
+}) {
+  // Draw small face
+  faceCtx.drawImage( // 5 source arguments
+  video, // Where source comes from
+  face.x, // Where the source pull starts on x-axis
+  face.y, // Where the source pull starts on y-axis
+  face.width, // Where to stop source pull on width
+  face.height, // Where to stop source pull on height
+  // 4 draw arguments
+  face.x, // Where to start drawing on x-axis
+  face.y, // Where to start drawing on y-axis
+  SIZE, // Where to stop drawing on width
+  SIZE // Where to stop drawing on height
+  ); // Take small face and draw back at normal size 
 }
 
 populateVideo().then(detect);
