40b70484b3bd06fc9b180beabf20ddf2febda513
Cassie Boyd <cassandra.bacon@gmail.com>
2020-09-08 14:51:42 -0500

    Looping over letter and index of input

--- exercises/sarcastic-text/text.js
+++ exercises/sarcastic-text/text.js
@@ -2,12 +2,23 @@ const textArea = document.querySelector('[name="text"]');
 const result = document.querySelector(".result");
 const filterInputs = document.querySelectorAll('[name="filter"]');
 
-console.log(textArea);
-console.log(result);
-console.log(filterInputs);
-
 function transformText(text) {
+    // Take the text and loop over each letter
+    const mod = Array.from(text).map(filters.sarcastic);
     result.textContent = text;
+}
+
+const filters = {
+    sarcastic: function(letter, index){ 
+        console.log(letter, index);
+        return letter;
+    },
+    funky: function(){ 
+
+    },
+    unable: function(){ 
+
+    },
 }
 
 textArea.addEventListener("input", e => transformText(e.target.value));
