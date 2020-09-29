6fa873bbd87160b45b6662f39d21a6145c743fc2
Cassie Boyd <cassandra.bacon@gmail.com>
2020-09-29 16:31:22 -0500

    Added gallery.js and displayed galleries to dom

---
+++ exercises/gallery/gallery.js
@@ -0,0 +1,8 @@
+function Gallery(gallery) {
+    if(!gallery) {
+        throw new Error("No Gallery Found!");
+    }
+}
+
+const gallery1 = Gallery(document.querySelector(".gallery1"));
+const gallery2 = Gallery(document.querySelector(".gallery2"));
