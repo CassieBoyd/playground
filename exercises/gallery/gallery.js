7c7e734c0ff0a63a299964e1b64f9609554f4ae6
Cassie Boyd <cassandra.bacon@gmail.com>
2020-09-30 17:03:05 -0500

    Selecting elements for gallery

--- exercises/gallery/gallery.js
+++ exercises/gallery/gallery.js
@@ -2,6 +2,13 @@ function Gallery(gallery) {
     if(!gallery) {
         throw new Error("No Gallery Found!");
     }
+
+    // Select elements we need
+    // Put in an array so they can be looped over
+    const images = Array.from(gallery.querySelectorAll("img"));
+    const modal = document.querySelector(".modal");
+    const prevButton = modal.querySelector(".prev");
+    const nextButton = modal.querySelector(".next");
 }
 
 const gallery1 = Gallery(document.querySelector(".gallery1"));
