d384fba386b6c1a41f2770c5f60c59583b046fac
Cassie Boyd <cassandra.bacon@gmail.com>
2020-09-30 17:11:13 -0500

    Event listeners added to images

--- exercises/gallery/gallery.js
+++ exercises/gallery/gallery.js
@@ -9,6 +9,22 @@ function Gallery(gallery) {
     const modal = document.querySelector(".modal");
     const prevButton = modal.querySelector(".prev");
     const nextButton = modal.querySelector(".next");
+
+    function showImage(el) {
+        if(!el) {
+            console.info("NO image to show");
+            return
+        }
+        // Update modal
+        console.log(el)
+    }
+
+    function handleImageClick(event) {
+        showImage(event.currentTarget);
+    }
+
+    // Looping over images array and adding event listeners
+    images.forEach(image => image.addEventListener("click", handleImageClick))
 }
 
 const gallery1 = Gallery(document.querySelector(".gallery1"));
